e7fbeb6997d8e6702af6535b3de81c3719f6c4aa
Mohammad Shahruk <46904275+786Mohammadshahruk@users.noreply.github.com>
2023-11-30 09:35:42 +0530

    Component Example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,37 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        //Creating and configuring the hibernate.cfg.xml
+        Configuration configuration = new Configuration();
+        configuration.configure();
+
+        //building the SessionFactory
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        //creating Session
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = session.beginTransaction();
+        session.persist(getEmployee());
+        transaction.commit();
+        session.close();
+
+    }
+
+    private static Employee getEmployee() {
+        Employee employee = new Employee();
+        employee.setFirstName("Amit");
+        employee.setLastName("Mishra");
+        employee.setEmail("amit.mishra@gmail.com");
+        return employee;
     }
 }
